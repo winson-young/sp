4ea8e0813b15c33cc7bbaa7eaf14998ebc5de962
2497303762@qq.com <2497303762@qq.com>
2017-06-30 01:29:32 +0800

    add the method which get task and component

--- System/Core/Sp.php
+++ System/Core/Sp.php
@@ -15,9 +15,28 @@ class Sp
 		// 访问路径
 		Route::initRoute();
         // 测试自动加载类
-        $task = TASK;
-        $component = '\\' . APP_NAME . '\\Component\\' . COMPONENT . 'Component';
-        $component = new $component();
-        $component->$task();
+        $task      = self::getTask();
+        $component = self::getComponent();
+        call_user_func(array(new $component, $task));
 	}
+
+    /**
+     * @desc 获取任务方法名
+     *
+     * @return string
+     */
+    private static function getTask()
+    {
+        return TASK;
+    }
+
+    /**
+     * @desc 获取组件类名
+     *
+     * @return string
+     */
+    private static function getComponent()
+    {
+        return '\\' . APP_NAME . '\\Component\\' . COMPONENT . 'Component';
+    }
 }
